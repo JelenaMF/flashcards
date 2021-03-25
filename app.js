3fb5590d73ffb58a0e0eddab21b98ea08662e57f
Jelena Feliciano <jfel217@gmail.com>
2021-03-25 14:50:11 -0400

    update
    redirecting to home page greeting with name input

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ app.use(cookieParser());
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) =>{
-    res.render('index');
+    const name = req.cookies.username;
+    res.render('index', {name});
 
 });
 
@@ -26,7 +27,7 @@ app.get('/sandbox', (req, res) =>{
 });
 
 app.get('/hello', (req, res) =>{
-    res.render('hello', {name: req.cookies.username});
+    res.render('hello');
 
 });
 
