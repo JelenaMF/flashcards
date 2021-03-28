c9c25b43a2103fcf37e9c5afa293da2f23f508a2
Jelena Feliciano <jfel217@gmail.com>
2021-03-28 11:26:42 -0400

    Update app.js
    body-parser was depreciated because Express has included it into it's program. replaced body-parser with the recommended express keyword

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 
 const app = express();
-
-app.use(bodyParser.urlencoded({extended: false}));
+/*body-parser is now apart of express so install this depency is no longer needed*/
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 
 app.set('view engine', 'pug');
