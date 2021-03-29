ae1d8164fa2d7017b1144601baef7743632c196a
Jelena Feliciano <jfel217@gmail.com>
2021-03-29 00:27:39 -0400

    Update app.js

--- app.js
+++ app.js
@@ -9,12 +9,13 @@ app.use(cookieParser());
 app.set('view engine', 'pug');
 
 app.use((req, res, next) => {
-    req.message = 'This message made it!';
+    console.log('hello')
     next();
 });
 
 app.use((req, res, next) => {
-    console.log(req.message);
+    console.log('world');
+    //const err = new Error('Oh no... Something went wrong.');
     next();
 });
 
