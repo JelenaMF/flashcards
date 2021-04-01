a1fe1c041faf63e541b30ea338a51eb4352dabfb
Jelena Feliciano <jfel217@gmail.com>
2021-03-31 23:36:33 -0400

    links
    created question/answer links that redirects to the question/answer card depending link clicked

--- routes/cards.js
+++ routes/cards.js
@@ -3,16 +3,31 @@ const router = express.Router();
 const {data} = require('../data/flashcardData.json');
 const {cards} = data;
 
+//create a route 
+router.get('/', (req, res) => {
+    const numberOfCards = cards.length;
+     //find a random card
+    const flashCardId = Math.floor(Math.random() * numberOfCards);
+    //redirect user to view random card.
+    res.redirect(`/cards/${flashCardId}?side=question`);
+    })
+
 router.get('/:id', (req, res) =>{
     const {side} = req.query;
     const {id} = req.params;
     const text = cards[id][side];
     const {hint} = cards[id];
     const templateData = {text};
-    
+/*when the question side of the card is displayed the hint and answer link is displayed */
     if(side === 'question') {
         templateData.hint = hint;
-    }
+        templateData.sideToShow = 'answer';
+        templateData.sideToShowDisplay = 'Answer';
+/** if card side is answer the hint is hidden and the question link is displayed*/
+    } else if(side === 'answer') {
+        templateData.sideToShow = 'question';
+        templateData.sideToShowDisplay = 'Question';
+    }   
     res.render('card', templateData);
 });
 
