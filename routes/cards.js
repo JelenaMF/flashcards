7bd7360f7bc7a4e53d5cbb3a8698e36e39e182c0
Jelena Feliciano <jfel217@gmail.com>
2021-04-01 21:43:33 -0400

    Update cards.js
    redirected route if no query string was present in the URL to add the query string

--- routes/cards.js
+++ routes/cards.js
@@ -9,15 +9,20 @@ router.get('/', (req, res) => {
      //find a random card
     const flashCardId = Math.floor(Math.random() * numberOfCards);
     //redirect user to view random card.
-    res.redirect(`/cards/${flashCardId}?side=question`);
+    res.redirect(`/cards/${flashCardId}`);
     })
 
 router.get('/:id', (req, res) =>{
     const {side} = req.query;
     const {id} = req.params;
+ //if input is not the side redirect to the card question    
+    if(!side){
+        res.redirect(`/cards/${id}?side=question`);
+    }
+
     const text = cards[id][side];
     const {hint} = cards[id];
-    const templateData = {text};
+    const templateData = {id, text};
 /*when the question side of the card is displayed the hint and answer link is displayed */
     if(side === 'question') {
         templateData.hint = hint;
